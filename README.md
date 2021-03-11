397699cb6e1ce58f5b3570357f007a863679a39b
Lukas Zaruba <lukas.zaruba@gmail.com>
2021-03-11 22:37:11 +0100

    Issue #7 - Add basic project structure
    Skipped files

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # sonar-scm-plugin
 
-mvn sonar:sonar   -Dsonar.projectKey=test   -Dsonar.host.url=http://localhost:9000 -Dsonar.analysis.host=https://api.github.com -Dsonar.analysis.owner=LZaruba -Dsonar.analysis.repo=sonar-scm-plugin -Dsonar.analysis.pullNumber=8 -Dsonar.analysis.token=[GITHUB_TOKEN] -Dsonar.analysis.username=lukas.zaruba -Dsonar.login=[TOKEN]
+## Usage
+### GitHub
+`mvn sonar:sonar -Dsonar.projectKey=test -Dsonar.host.url=http://localhost:9000 -Dsonar.analysis.host=https://api.github.com -Dsonar.analysis.owner=LZaruba -Dsonar.analysis.repo=sonar-scm-plugin -Dsonar.analysis.pullNumber=[PR_NUMBER] -Dsonar.analysis.token=[GITHUB_TOKEN] -Dsonar.analysis.scm=github -Dsonar.analysis.username=[GITHUB_USERNAME] -Dsonar.login=[TOKEN]`
+
+## Contributing
+* Follow the [Google code style guide](https://google.github.io/styleguide/javaguide.html).
+* Make sure that your change contain proper test coverage
+* Separate API (Interface) from implementation (Class Implementation)
+* Add license header and class header based on the example of AnalysisProcessor class
 
---
+++ src/main/java/cz/lzaruba/sonar/scm/providers/impl/SCMProviderFactoryImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Lukas Zaruba, lukas.zaruba@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.lzaruba.sonar.scm.providers.impl;
+
+import cz.lzaruba.sonar.scm.providers.SCMProvider;
+import cz.lzaruba.sonar.scm.providers.SCMProviderFactory;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.function.Function.identity;
+import static java.util.stream.Collectors.toMap;
+
+/**
+ * @author Lukas Zaruba, lukas.zaruba@gmail.com, 2021
+ */
+public class SCMProviderFactoryImpl implements SCMProviderFactory {
+
+    private static final List<SCMProvider> PROVIDERS = Arrays.asList(
+            new BitbucketServerSCMProvider(),
+            new GitHubSCMProvider());
+
+    private final Map<String, SCMProvider> providersMap;
+
+    public SCMProviderFactoryImpl() {
+        providersMap = PROVIDERS.stream().collect(toMap(SCMProvider::getId, identity()));
+    }
+
+    @Override
+    public SCMProvider getProvider(String providerId) {
+        SCMProvider provider = providersMap.get(providerId);
+        if (provider == null) {
+            throw new IllegalArgumentException("There is no provider registered for id " + providerId
+                    + ". Available ids are: " + providersMap.keySet());
+        }
+        return provider;
+    }
+
+}
